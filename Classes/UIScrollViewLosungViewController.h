68f3ed98ed98cdbe3c91cb256ed54df39413c0b8
Stephan Meissner <atitsbest@gmail.com>
2010-12-29 12:40:46 +0100

    Der Wechsel auf den aktuellen Tag funktioniert auch über Jahresgrenzen.

--- Classes/UIScrollViewLosungViewController.h
+++ Classes/UIScrollViewLosungViewController.h
@@ -15,21 +15,29 @@
 	
 	UIScrollView *pagingScrollView;
 	UIImageView *splashView;
+
+	// Cache für die Controls.
 	NSMutableSet *visiblePages;
 	NSMutableSet *recycledPages;
+	
+	// Outlets
 	IBOutlet LosungView *losungView;
 	IBOutlet UIView *firstYearView;
 	IBOutlet UIView *lastYearView;
+
+	// Der Kalender, der im gesamten Controler verwendet wird.
+	NSCalendar *calender;
 }
 
 @property(nonatomic, retain) LosungView *losungView;
 @property(nonatomic, retain) UIView *firstYearView;
 @property(nonatomic, retain) UIView *lastYearView;
+@property(nonatomic, retain) NSCalendar *calender;
 
 - (void)tileLosungViews; // Nur die nötigen Losungen anzeigen.
 - (BOOL)isDisplayingLosungForIndex:(NSInteger)index; // Wird die Losung mit dem Index gerade angezeigt?
+- (void)scrollToDate:(NSDate*)date;
 - (void)scrollToToday;
-- (void)scrollToLastDayOfYear:(NSUInteger)year;
 - (void)scrollToDayOfYear:(NSUInteger)dayOfYear;
 - (void)setupForYear:(NSInteger)year;
 - (LosungView*)dequeueRecycledPage;
--- Classes/UIScrollViewLosungViewController.m
+++ Classes/UIScrollViewLosungViewController.m
@@ -12,9 +12,17 @@
 #import "UIScrollViewLosungViewController.h"
 #import "Views/LosungView.h"
 
+// Untersützte Jahre.
+#define FIRST_YEAR	2010
+#define LAST_YEAR	2011
+
+
 @implementation UIScrollViewLosungViewController
 
-@synthesize losungView, firstYearView, lastYearView;
+@synthesize losungView, 
+			firstYearView, 
+			lastYearView,
+			calender;
 
 // Rotierung konfigurieren.
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)toInterfaceOrientation {
@@ -30,6 +38,10 @@
 	// Init.
 	visiblePages = [[NSMutableSet alloc] init];
 	recycledPages = [[NSMutableSet alloc] init];
+	
+	// Kalender erstellen.
+	self.calender = [[NSCalendar alloc] initWithCalendarIdentifier:NSGregorianCalendar];
+	[self.calender setTimeZone:[NSTimeZone timeZoneWithAbbreviation:@"GMT"]];
 
     // Doppel-Tab überprüfen.
 /*    UITapGestureRecognizer* doubleTap = [[UITapGestureRecognizer alloc] initWithTarget:self 
@@ -65,13 +77,17 @@
 }
 
 - (void)startupShowAnimationDone:(NSString *)animationID finished:(NSNumber *)finished context:(void *)context {
-	NSNumber *direction = (NSNumber*)context;
-	NSInteger year = [ApplicationContext current].currentYear + [direction intValue];
+	NSDate *date = (NSDate*)context;
+	// Das Jahr ermitteln.
+	NSDateComponents *components = [self.calender components:NSYearCalendarUnit fromDate:date];
+	NSInteger year = [components year];    	
+	
+	// Jahr wechseln.
 	[self setupForYear:year];
-	if ([direction intValue] == 1) {
-		[self scrollToLastDayOfYear:year];
-	}
-	[direction release];
+	[self scrollToDate:date]; // Jetzt wo das Jahr gesetzt ist, kann direkt zum Tage gescrollt werden.
+
+    [date release];
+
     [UIView beginAnimations:nil context:nil];
     [UIView setAnimationDuration:.7];
     [UIView setAnimationTransition:UIViewAnimationTransitionNone forView:pagingScrollView.window cache:YES];
@@ -82,21 +98,20 @@
     [UIView commitAnimations];
 }
 
-- (void)changeYear:(NSInteger)direction {
+- (void)changeYear:(NSDate*)date {
 	if (splashView != nil) return;
     splashView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 320, 480)];
     splashView.image = [UIImage imageNamed:@"Default.png"];
 	splashView.alpha = 0.0;
     [pagingScrollView.window addSubview:splashView];
     [pagingScrollView.window bringSubviewToFront:splashView];
-    [UIView beginAnimations:nil context:[[NSNumber numberWithInt:direction] retain]];
+    [UIView beginAnimations:nil context:[date retain]];
     [UIView setAnimationDuration:.7];
     [UIView setAnimationTransition:UIViewAnimationTransitionNone forView:pagingScrollView.window cache:YES];
     [UIView setAnimationDelegate:self];
     [UIView setAnimationDidStopSelector:@selector(startupShowAnimationDone:finished:context:)];
 	splashView.alpha = 1.0;
     [UIView commitAnimations];
-//	[self setupForYear:[ApplicationContext current].currentYear + direction];
 }
 
 
@@ -131,8 +146,15 @@
 	for (int index=firstNeededPageIndex; index<=lastNeededPageIndex; index+=1) {
 		if (![self isDisplayingLosungForIndex:index]) {
 			if (index == ([[ApplicationContext current].losungen count] + 1)) {
-				if ([ApplicationContext current].currentYear < 2011) { 
-					[self changeYear:+1]; 
+				if ([ApplicationContext current].currentYear < LAST_YEAR) { 
+					NSDateComponents *comps = [[NSDateComponents alloc] init];
+					[comps setDay:1];
+					[comps setMonth:1];
+					[comps setYear:[ApplicationContext current].currentYear+1];
+					[comps setHour:1];
+					NSDate *date = [self.calender dateFromComponents:comps];
+					[self scrollToDate:date];
+					[comps release];
 				}
 				else {
 					if (self.lastYearView == nil) {
@@ -148,8 +170,14 @@
 				}
 			}
 			else if (index == 0) {
-				if ([ApplicationContext current].currentYear > 2010) { 
-					[self changeYear:-1]; 
+				if ([ApplicationContext current].currentYear > FIRST_YEAR) { 
+					NSDateComponents *comps = [[NSDateComponents alloc] init];
+					[comps setDay:31];
+					[comps setMonth:12];
+					[comps setYear:[ApplicationContext current].currentYear-1];
+					NSDate *date = [self.calender dateFromComponents:comps];
+					[self scrollToDate:date];
+					[comps release];
 				}
 				else {
 					if (self.firstYearView == nil) {
@@ -177,42 +205,41 @@
 									
 }
 
-/**
- * Scrollt die Losungen zum heutigen Tag.
- */
+// Scrollt zu dem angegebenen Datum. Dabei kann auch ein Jahreswechsel nötig sein.
+- (void)scrollToDate:(NSDate *)date {
+	// Das Jahr ermitteln.
+	NSDateComponents *components = [self.calender components:NSYearCalendarUnit fromDate:date];
+	NSInteger year = [components year];    	
+	
+	// Gültiges Jahr?
+	if (year == FIRST_YEAR || year == LAST_YEAR) {
+		// Jahr wechseln?
+		if ([ApplicationContext current].currentYear != year) {
+			[self changeYear:date];
+		}
+		else {
+			NSUInteger dayOfYear =[self.calender ordinalityOfUnit:NSDayCalendarUnit
+														   inUnit:NSYearCalendarUnit 
+														  forDate:date];
+			[self scrollToDayOfYear:dayOfYear];
+		}
+
+	}
+	else {
+		// Wenn nicht, dann wird eine Meldung ausgegeben und die erste Losunge
+		// des ersten, gültigen Jahres angezeigt.
+		[self scrollToDayOfYear:1];
+	}
+}
+
+
+// Scrollt die Losungen zum heutigen Tag.
 - (void) scrollToToday {
-	// Zum aktuellen Tag scrollen.
-	NSCalendar *gregorian = [[[NSCalendar alloc] initWithCalendarIdentifier:NSGregorianCalendar] autorelease];
-	NSUInteger dayOfYear =[gregorian ordinalityOfUnit:NSDayCalendarUnit
-											   inUnit:NSYearCalendarUnit 
-											  forDate:[NSDate date]];
-	[self scrollToDayOfYear:dayOfYear];
+	[self scrollToDate:[NSDate date]];
 }
 
-/**
- * Scrollt die Losungen zum letzten Tag des Jahres.
- */
-- (void) scrollToLastDayOfYear:(NSUInteger)year {
-	// 31. 12. xxxx
-	NSDateFormatter *formatter = [[NSDateFormatter alloc]init]; 
-	[formatter setDateFormat:@"yyyy-MM-dd"];
-	NSString *dateString = [NSString stringWithFormat:@"%i-12-31", year];
-	NSDate *date = [formatter dateFromString:dateString];
 
-	// Zum aktuellen Tag scrollen.
-	NSCalendar *gregorian = [[[NSCalendar alloc] initWithCalendarIdentifier:NSGregorianCalendar] autorelease];
-	NSUInteger dayOfYear =[gregorian ordinalityOfUnit:NSDayCalendarUnit
-											   inUnit:NSYearCalendarUnit 
-											  forDate:date];
-	[self scrollToDayOfYear:dayOfYear];
-
-	// Aufräumen.
-	[formatter release];
-}
-
-/**
- * Scrollt die Losungen zum angegebenen Tag.
- */
+// Scrollt die Losungen zum angegebenen Tag.
 - (void) scrollToDayOfYear:(NSUInteger)dayOfYear {
 	CGRect pagingScrollViewFrame = [[UIScreen mainScreen] bounds];
 	[pagingScrollView setContentOffset:CGPointMake(CGRectGetWidth(pagingScrollViewFrame) * dayOfYear, 0) 
@@ -288,6 +315,7 @@
 	[visiblePages release];
 	[recycledPages release];
 	[losungView release];
+	[calender release];
 	[super dealloc];
 }
 
